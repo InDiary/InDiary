0572d0ee9a2daf97ace80dd11bcd82b636b73c73
InDiary <indiary.app@gmail.com>
2013-08-09 02:17:20 +0800

    Restricted date search range so that start date does not exceed end date and vice versa.

--- Resources/ui/handheld/DatetimeRangeView.js
+++ Resources/ui/handheld/DatetimeRangeView.js
@@ -15,19 +15,24 @@ function DatetimeRangeView(vars) {
         backgroundColor: 'black',
         backgroundSelectedColor: 'black',
         touchEnabled: false,
-        value: vars.value
+        value: vars.value.slice(0)
     });
 
     var fromFieldView = new EntryFieldView({
         type : 'datetime',
         name : L('start') + ' ' + vars.name,
-        value : vars.value[0],
+        value : self.value[0],
         dialogTitle : vars.name
     });
     self.add(fromFieldView);
     fromFieldView.addEventListener('change', function(e) {
-        self.value[0] = e.value;
-        self.fireEvent('change', {value: self.value});
+    	var over_range = (e.value > self.value[1]);
+        self.value[0] = (!over_range) ? e.value : self.value[1];
+        if (over_range){
+        	fromFieldView.value = self.value[0];
+        	fromFieldView.fireEvent('change', {value: fromFieldView.value});
+        }
+        self.fireEvent('change', {value: self.value.slice(0)});
     });
     
     self.add(Ti.UI.createView({
@@ -39,13 +44,18 @@ function DatetimeRangeView(vars) {
     var toFieldView = new EntryFieldView({
         type : 'datetime',
         name : L('end') + ' ' + vars.name,
-        value : vars.value[1],
+        value : self.value[1],
         dialogTitle : vars.name
     });
     self.add(toFieldView);
     toFieldView.addEventListener('change', function(e) {
-        self.value[1] = e.value;
-        self.fireEvent('change', {value: self.value});
+    	var under_range = (e.value < self.value[0]);
+        self.value[1] = (!under_range) ? e.value : self.value[0];
+        if (under_range){
+        	toFieldView.value = self.value[1];
+        	toFieldView.fireEvent('change', {value: toFieldView.value});
+        }
+        self.fireEvent('change', {value: self.value.slice(0)});
     });
     
     return self;
