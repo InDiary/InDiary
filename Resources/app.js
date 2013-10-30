74f63bed369ce08acbf4d5eaf8a0a69af22e35ac
InDiary <indiary.app@gmail.com>
2013-10-30 17:24:00 +0800

    Removed clutter from app.js

--- Resources/app.js
+++ Resources/app.js
@@ -1,34 +1,20 @@
-/*
- * A tabbed application, consisting of multiple stacks of windows associated with tabs in a tab group.  
- * A starting point for tab-based application with multiple top-level windows. 
- * Requires Titanium Mobile SDK 1.8.0+.
- * 
- * In app.js, we generally take care of a few things:
- * - Bootstrap the application with any data we need
- * - Check for dependencies like device type, platform version or network connection
- * - Require and open our top-level UI component
- *  
- */
-
-//bootstrap and check dependencies
 if (Ti.version < 1.8 ) {
-	alert('Sorry - this application template requires Titanium Mobile SDK 1.8 or later');
+	alert('Titanium Mobile SDK 1.8 or later required');
 }
 
-// This is a single context application with mutliple windows in a stack
 (function() {
-	//determine platform and form factor and render appropriate components
+    var db = require('db');
+
 	var osname = Ti.Platform.osname,
 		version = Ti.Platform.version,
 		height = Ti.Platform.displayCaps.platformHeight,
 		width = Ti.Platform.displayCaps.platformWidth;
 	
-	//considering tablet to have one dimension over 900px - this is imperfect, so you should feel free to decide
-	//yourself what you consider a tablet form factor for android
-	var isTablet = osname === 'ipad' || (osname === 'android' && (width > 899 || height > 899));
-
-    var db = require('db');
+	var isTablet = osname === 'ipad' ||
+        (osname === 'android' && (width > 899 || height > 899));
+    
 	db.createDatabase();
+    
 	var EntryListWin = require('ui/handheld/EntryListWin');
 	new EntryListWin().open();
 })();
